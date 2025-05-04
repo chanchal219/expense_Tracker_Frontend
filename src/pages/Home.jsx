c34419521f2d8250224aff817b9a2cb9ecdfb271
chanchal219 <chanchal092228@gmail.com>
2025-05-05 02:07:51 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,128 @@
+import React ,{useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { handleError, handleSuccess } from '../utils'
+import ExpensesTable from './ExpensesTable'
+import ExpenseTrackerForm from './ExpenseTrackerForm'
+import ExpenseDetails from './ExpenseDetails'
+
+const Home = () => {
+    const [loggedInUser,setLoggedInUser]=useState('')
+  const [expenses,setExpenses]=useState([]);
+  const [expenseAmt,setExpenseAmt]=useState(0);
+  const [incomeAmt,setIncomeAmt]=useState(0)
+   
+    const navigate=useNavigate()
+    useEffect(()=>{
+       setLoggedInUser( localStorage.getItem('loggedInUser'))
+    },[])
+
+    useEffect(()=>{
+const amounts=expenses.map((item)=>item.amount)
+console.log(amounts)
+
+const income=amounts.filter(item=>item>0)
+.reduce((acc,item)=>(acc+=item),0);
+console.log('income:',income)
+
+const exp=amounts.filter(item=>item<0)
+.reduce((acc,item)=>(acc+=item),0)*-1;
+console.log('exp:',exp)
+setIncomeAmt(income);
+setExpenseAmt(exp)
+    },[expenses])
+
+    
+
+    const handleLogout=(e)=>{
+localStorage.removeItem('token')
+localStorage.removeItem('loggedInUser')
+handleSuccess('User logged out')
+setTimeout(()=>{
+navigate('/login')
+},1000)
+    }
+    const fetchExpenses=async ()=>{
+        try{
+const url='http://localhost:8080/expenses'
+const headers={
+    headers:{'Authorization':localStorage.getItem('token')}
+}
+const response=await fetch(url,headers);
+if(response.status===403){
+    navigate('/login');
+    return;
+}
+const result=await response.json()
+console.log(result.data)
+setExpenses(result.data)
+        }catch(err){
+            handleError(err);
+        }
+    }
+
+    const addExpenses=async (data)=>{
+        try{
+const url='http://localhost:8080/expenses'
+const headers={
+    headers:{'Authorization':localStorage.getItem('token'),
+        'Content-Type':'application/json'
+    },method:'POST',
+    body:JSON.stringify(data)
+}
+const response=await fetch(url,headers);
+if(response.status===403){
+    navigate('/login');
+    return;
+}
+const result=await response.json()
+console.log(result.data)
+setExpenses(result.data)
+handleSuccess(result.message)
+        }catch(err){
+            handleError(err);
+        }
+    }
+    useEffect(()=>{
+        fetchExpenses()
+    },[])
+
+    const handleDeleteExpense=async (expenseId)=>{
+        try{
+            const url=`http://localhost:8080/expenses/${expenseId}`
+            const headers={
+                headers:{'Authorization':localStorage.getItem('token'),
+                    'Content-Type':'application/json'
+                },method:'DELETE',
+                
+            }
+            const response=await fetch(url,headers);
+            if(response.status===403){
+                navigate('/login');
+                return;
+            }
+            const result=await response.json()
+            console.log(result.data)
+            setExpenses(result.data)
+            handleSuccess(result.message)
+                    }catch(err){
+                        handleError(err);
+                    }
+                
+    }
+  return (
+    <div>
+        <div className='user-section'>
+            <h1>{loggedInUser}</h1>
+        <button onClick={handleLogout}>Logout</button>
+        </div>
+        <ExpenseDetails incomeAmt={incomeAmt} 
+        expenseAmt={expenseAmt}/>
+        <ExpenseTrackerForm addExpenses={addExpenses}/>
+        <ExpensesTable expenses={expenses} handleDeleteExpense={handleDeleteExpense}/>
+        <ToastContainer/>
+    </div>
+  )
+}
+
+export default Home
